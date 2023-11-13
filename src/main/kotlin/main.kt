376ab9a107d760ab08b32c12c36b5d8355bc5acf
Ekaterina Safonova <katerina_s92@mail.ru>
2023-11-14 03:00:42 +0700

    Avtotests added and README.md changed

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,7 @@
 fun main() {
     val amount = 5000
     val previouslyTransferredAmount = 0
-    val cardType = "Visa/Mir"
+    val cardType = "Visa"
     val feePerOperation = feeCalculation(cardType, previouslyTransferredAmount, amount)
 
     println("Комиссия за перевод стоставит: $feePerOperation руб.")
@@ -13,13 +13,13 @@ fun feeCalculation(cardType: String, previouslyTransferredAmount: Int, amount: I
     val feeForMaestro = 0.006
 
     if (cardType == "VK Pay" && (amount > 15_000 || previouslyTransferredAmount > 40_000)) return -666
-    if ((cardType == "Maestro/Mastercard" || cardType == "Visa/Mir") &&
+    if ((cardType == "Maestro" || cardType == "Mastercard" || cardType == "Visa" || cardType == "Mir") &&
         (amount > 150_000 || previouslyTransferredAmount > 600_000)
     ) return -666
 
     var fee = when (cardType) {
-        "Maestro/Mastercard" -> if (amount < 75_000) 0 else amount * feeForMaestro + 20
-        "Visa/Mir" -> if ((amount * feeForVisa) > minFeeAmountForVisa) amount * feeForVisa else minFeeAmountForVisa
+        "Maestro", "Mastercard" -> if (amount < 75_000) 0 else amount * feeForMaestro + 20
+        "Visa", "Mir" -> if ((amount * feeForVisa) > minFeeAmountForVisa) amount * feeForVisa else minFeeAmountForVisa
         else -> 0
     }
     return fee.toInt()
---
+++ src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,89 @@
+import org.junit.Assert.*
+import org.junit.Test
+
+class MainKtTest {
+
+    @Test
+    fun feeCalculationForVisa() {
+        val result = feeCalculation(cardType = "Visa", previouslyTransferredAmount = 0, amount = 5000)
+        assertEquals(37, result)
+    }
+
+    @Test
+    fun feeCalculationForVisaWithMinFee() {
+        val result = feeCalculation(cardType = "Visa", previouslyTransferredAmount = 0, amount = 1000)
+        assertEquals(35, result)
+    }
+
+    @Test
+    fun feeCalculationForVisaWithExtraSum() {
+        val result = feeCalculation(cardType = "Visa", previouslyTransferredAmount = 601_000, amount = 152_000)
+        assertEquals(-666, result)
+    }
+
+    @Test
+    fun feeCalculationForMir() {
+        val result = feeCalculation(cardType = "Mir", previouslyTransferredAmount = 0, amount = 5000)
+        assertEquals(37, result)
+    }
+
+    @Test
+    fun feeCalculationForMirWithMinFee() {
+        val result = feeCalculation(cardType = "Mir", previouslyTransferredAmount = 0, amount = 1000)
+        assertEquals(35, result)
+    }
+
+    @Test
+    fun feeCalculationForMirWithExtraSum() {
+        val result = feeCalculation(cardType = "Mir", previouslyTransferredAmount = 620_000, amount = 180_000)
+        assertEquals(-666, result)
+    }
+
+    @Test
+    fun feeCalculationForMaestro() {
+        val result = feeCalculation(cardType = "Maestro", previouslyTransferredAmount = 0, amount = 76_000)
+        assertEquals(476, result)
+    }
+
+    @Test
+    fun feeCalculationForMaestroWithZeroFee() {
+        val result = feeCalculation(cardType = "Maestro", previouslyTransferredAmount = 0, amount = 1000)
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun feeCalculationForMaestroWithExtraSum() {
+        val result = feeCalculation(cardType = "Maestro", previouslyTransferredAmount = 610_000, amount = 195_000)
+        assertEquals(-666, result)
+    }
+
+    @Test
+    fun feeCalculationForMastercard() {
+        val result = feeCalculation(cardType = "Mastercard", previouslyTransferredAmount = 0, amount = 76_000)
+        assertEquals(476, result)
+    }
+
+    @Test
+    fun feeCalculationForMastercardWithZeroFee() {
+        val result = feeCalculation(cardType = "Mastercard", previouslyTransferredAmount = 0, amount = 1000)
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun feeCalculationForMastercardWithExtraSum() {
+        val result = feeCalculation(cardType = "Mastercard", previouslyTransferredAmount = 987_000, amount = 200_000)
+        assertEquals(-666, result)
+    }
+
+    @Test
+    fun feeCalculationForVKPay() {
+        val result = feeCalculation(cardType = "VK Pay", previouslyTransferredAmount = 30_000, amount = 8000)
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun feeCalculationForVKPayWithExtraSum() {
+        val result = feeCalculation(cardType = "VK Pay", previouslyTransferredAmount = 45_000, amount = 16_000)
+        assertEquals(-666, result)
+    }
+}
